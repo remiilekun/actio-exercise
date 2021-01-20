0d6634a11d0a48a7215ec2a7827d8687f2b1f978
Remilekun Salami <remilekun.salami@gmail.com>
2021-01-20 17:16:04 +0100

    feat: tab router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,39 @@
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '@screens/Home';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '@screens/Home';
+import HomeIcon from '@assets/svg/home.svg';
+import CalendarIcon from '@assets/svg/calendar.svg';
+import GridIcon from '@assets/svg/grid.svg';
+import MenuIcon from '@assets/svg/menu.svg';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-function App() {
+const Router = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  );
-}
+    <Tab.Navigator
+      tabBarOptions={{
+        style: { backgroundColor: '#000' },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarLabel: ({ color }) => null,
 
-export default App;
+        tabBarIcon: () => {
+          const icons = {
+            Home: <HomeIcon />,
+            Grid: <GridIcon />,
+            Calendar: <CalendarIcon />,
+            Menu: <MenuIcon />,
+          };
+          return icons[route.name];
+        },
+      })}
+    >
+      <Tab.Screen component={Home} name="Home" />
+      <Tab.Screen component={Home} name="Grid" />
+      <Tab.Screen component={Home} name="Calendar" />
+      <Tab.Screen component={Home} name="Menu" />
+    </Tab.Navigator>
+  );
+};
+
+export default Router;
