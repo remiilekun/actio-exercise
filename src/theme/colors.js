2d7c020a3ef1672fd226b6cce29fc5a81a0c1fc7
Remilekun Salami <remilekun.salami@gmail.com>
2021-01-20 13:49:30 +0100

    feat: setup theme

---
+++ src/theme/colors.js
@@ -0,0 +1,24 @@
+export const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  cyan: {
+    100: '#58D7D9',
+    200: '#39CCD0',
+  },
+  grey: {
+    100: '#CCCCCC',
+    200: '#444444',
+    300: '#222222',
+  },
+  yellow: '#FFD800',
+  lemon: '#83F6A1',
+  pink: {
+    100: '#FF5D95',
+    200: '#D8306B',
+  },
+  purple: {
+    100: '#C393FF',
+    200: '#9E5DFA',
+    300: '#903BFF',
+  },
+};
